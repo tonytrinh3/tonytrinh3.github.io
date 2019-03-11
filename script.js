8598166d89dcf7b96a7a174d6b204137c0c0af38
Tony Trinh <tony.w.trinh3@gmail.com>
2019-03-11 01:28:16 -0700

    dropdown menu

--- script.js
+++ script.js
@@ -39,6 +39,30 @@ const bar = d3.select('#chart')
        return "translate(0, " + i * barHeight + ")"
     });//this transform and translate is needed to get one bar and loop it through all data
 
+//this is to create the drop down for the buildings picture in "calibrated energy model"
+const thermalBlock = [
+    {space: "Conditioning Systems", href: "https://elementa.nyc/projects/yale-berkeley-college/img/9_ZoneDiagrams_190205_Conditioning_Systems-01.jpg"}, 
+    {space: "Program", href: "https://elementa.nyc/projects/yale-berkeley-college/img/ZoneDiagrams_190205_Program-01.jpg"},
+    {space: "Envelope", href: "https://elementa.nyc/projects/yale-berkeley-college/img/00_ZoneDiagrams_190205_Envelope-01.jpg"},
+    {space: "Bedrooms", href: "https://elementa.nyc/projects/yale-berkeley-college/img/0_ZoneDiagrams_190205_Bedroom-01.jpg"},
+    {space: "Cafe and Dining", href: "https://elementa.nyc/projects/yale-berkeley-college/img/1_ZoneDiagrams_190205_CafeDining-01.jpg"}
+    ]   
+
+d3.select('#thermal-block')
+    .append("select")//this is the select from html when wanting to start creating a dropdown
+    .selectAll("option")//this is the individual options within the <select></select>. for all option, attach on the data below?
+    .data(thermalBlock)//data associated with each option
+    .enter() //this starts the loop
+    .append("option") // loop each option and add on to selection
+    .attr("value", function(d){ //add on value (html standard) on to each option
+        return d.space;
+    })
+    .text(function(d){ //add the associated text to each value
+        return d.space;
+    });
+
+
+
 
 const data0 = [
     { gpa: 3.42, height: 138 },
